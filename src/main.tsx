59582a0393c84d8355fb6806554b7723d7eb1561
Diana <dinessa.ga@gmail.com>
2026-09-11 10:29:40 -0500

    feat: add ScrollToTop component to manage scroll position on route changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,16 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { ScrollToTop } from "./components/ScrollToTop";
 import "./index.css";
+
+if ('scrollRestoration' in window.history) {
+  window.history.scrollRestoration = 'manual';
+}
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
